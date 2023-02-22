fcbdbe794a78b0a6fd27df4a153717274e5058a6
ADMIN <marat.bagautdinov522@mail.ru>
2023-02-22 17:11:14 +0300

    deploy 1.3

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import colors from 'colors'
 import cors from 'cors'
 import mongoose from 'mongoose'
 // Routes
@@ -11,8 +10,8 @@ dotenv.config()
 
 mongoose
 	.connect(process.env.MONGO_URI)
-	.then(() => console.log(`MongoDB connected: ${process.env.MONGO_URI}`.cyan))
-	.catch(err => console.log(`Error: ${err.message}`.red.underline.bold))
+	.then(() => console.log(`MongoDB connected: ${process.env.MONGO_URI}`))
+	.catch(err => console.log(`Error: ${err.message}`))
 const app = express()
 
 
@@ -27,6 +26,6 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () =>
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
 	)
 )
