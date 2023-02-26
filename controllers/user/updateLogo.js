2131ccc77681cc5b53045f9459e65f5803bdb5c7
ADMIN <marat.bagautdinov522@mail.ru>
2023-02-26 22:58:39 +0300

    upload files 2.2

--- controllers/user/updateLogo.js
+++ controllers/user/updateLogo.js
@@ -3,6 +3,7 @@ import User from '../../models/userModel.js'
 
 export default asyncHandler(async (req, res) => {
 	const user = await User.findById(req.userID)
+	console.log(`Img data: ${req}`)
 	user.images = { ...user.images, logo: req.body.title }
 	user.save()
 	res.json({ logo: user.images })
